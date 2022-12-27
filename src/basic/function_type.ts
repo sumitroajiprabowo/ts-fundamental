bc29c72b45c957af55124949236bc57160ec302e
Sumitro Aji Prabowo <bowo@anakdesa.id>
2022-12-27 07:51:32 +0700

    Update basic

---
+++ src/basic/function_type.ts
@@ -0,0 +1,8 @@
+/* Function Types */
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add2;
+// combineValues = printResult2;
+// combineValues = 5;
+
+console.log(combineValues(8, 8));
---
+++ src/basic/unknow_type.ts
@@ -0,0 +1,11 @@
+/* Unknown Type */
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = 'Dawu';
+if (typeof userInput === 'string') {
+    userName = userInput;
+} else {
+    userName = 'Dawu';
+}
